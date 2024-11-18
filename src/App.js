2a9ca3aa8bfba6cb92f38a2ba52c2dbf98f120a4
nehalskdev <nikesk1999@gmail.com>
2024-11-18 10:19:25 +0530

    added static jsx

---
+++ src/App.js
@@ -0,0 +1,16 @@
+export default function App() {
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+      <p className="message">Hello</p>
+      <div className="buttons">
+        <button>Prev</button>
+        <button>Next</button>
+      </div>
+    </div>
+  );
+}
