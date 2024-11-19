0c430faf8320428674ed97afca8c5df6cb638757
nehalskdev <nikesk1999@gmail.com>
2024-11-19 08:52:47 +0530

    added functionality using useState

--- src/App.js
+++ src/App.js
@@ -12,15 +12,19 @@ export default function App() {
 
   // const step = 1;
 
-  function handlePrev() {}
-  function handleNext() {}
+  function handlePrev() {
+    if (step > 1) setStep(step - 1);
+  }
+  function handleNext() {
+    if (step < 3) setStep(step + 1);
+  }
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
       <p className="message">
         Step {step}: {messages[step - 1]}
@@ -32,7 +36,10 @@ export default function App() {
         >
           Prev
         </button>
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
